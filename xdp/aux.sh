7752d0720cda5172b1a54b72088dc7990d92fb69
igallar98 <43842255+igallar98@users.noreply.github.com>
2020-11-01 16:29:45 +0100

    Editar script de inicio

--- xdp/aux.sh
+++ xdp/aux.sh
@@ -1,7 +1,7 @@
 sudo mount -t bpf bpf /sys/fs/bpf/
 ulimit -l unlimited
-sudo ./xdp_loader --auto-mode --dev $(echo | route | grep '^default' | grep -o '[^ ]*$') --force --progsec xdp_pass
-sudo ./xdp_stats --dev $(echo | route | grep '^default' | grep -o '[^ ]*$') &
+sudo ./xdp_loader --auto-mode --dev $(echo | route | grep '^default' | grep -o '[^ ]*$' | head -n 1) --force --progsec xdp_pass
+sudo ./xdp_stats --dev $(echo | route | grep '^default' | grep -o '[^ ]*$' | head -n 1) &
 
 cd ../interface
 sudo python3 run.py
